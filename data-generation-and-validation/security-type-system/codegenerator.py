da6a4bd578c8cd56b383f0da5d72ba44af65f128
MadeInGer <30792417+MadeInGer@users.noreply.github.com>
2018-10-27 11:53:40 +0200

    Refactor Codegenerator and AST Layout #108 #110

--- data-generation-and-validation/security-type-system/codegenerator.py
+++ data-generation-and-validation/security-type-system/codegenerator.py
@@ -1,260 +1,246 @@
 import random
 import string
+import sys
 from random import randint
 from pprint import pprint
+
+sys.setrecursionlimit(1500)
 
 
 IDENTIFIER_LENGTH = 1
 INT_START_RANGE = -999999
 INT_END_RANGE = 999999
 
-MAX_DEPTH_EXPRESSION = 10
-MAX_DEPTH_COMMAND = 10
+MAX_EXPRESSIONS_PER_EXPRESSION = 7
+MAX_COMMANDS_PER_COMMAND = 15  # successfully tested: 1000
 
-depth_expression = 0  # global expression depth counter
-depth_command = 0  # global command depth counter
-
-
-class Expr:
-    kind = None
-
-    def __init__(self, kind):
-        self.kind = kind
+ENABLE_SEED = True
 
 
-class TypeExpr(Expr):
-    _value = None
+if ENABLE_SEED:
+    SEED = 3
+    random.seed(SEED)
+    print('SEED: {}'.format(SEED))
 
-    def __init__(self, kind, value):
-        Expr.kind = kind
-        self._value = value
 
-    def get(self):
-        return {'Kind': Expr.kind,
-                'Value': self._value
+class IntExpr:
+
+    def gen(self):
+        return {'Kind': 'Int',
+                'Value': randint(INT_START_RANGE, INT_END_RANGE)
                 }
 
 
-class OpExpr(Expr):
-    _left = None
-    _right = None
+class VarExpr:
 
-    def __init__(self, kind, left, right):
-        Expr.kind = kind
-        self._left = left
-        self._right = right
-
-    def get(self):
-        return {'Kind': Expr.kind,
-                'Left': self._left,
-                'Right': self._right,
+    def gen(self):
+        return {'Kind': 'Var',
+                'Name': ''.join(random.choice(string.ascii_letters)
+                                for _ in range(IDENTIFIER_LENGTH))
                 }
 
 
-class ExpressionGenerator:
+class AddExpr:
 
-    ast = {}
+    def gen(self, left, right):
+        return {'Kind': 'Add',
+                'Left': left,
+                'Right': right,
+                }
 
-    def __init__(self):
-        rnd = randint(0, 1)
-        if rnd == 0:
-            self.ast = self.int().get()
+
+class SubExpr:
+
+    def gen(self, left, right):
+        return {'Kind': 'Sub',
+                'Left': left,
+                'Right': right,
+                }
+
+
+class LessExpr:
+
+    def gen(self, left, right):
+        return {'Kind': 'Less',
+                'Left': left,
+                'Right': right,
+                }
+
+
+class EqualExpr:
+
+    def gen(self, left, right):
+        return {'Kind': 'Equal',
+                'Left': left,
+                'Right': right,
+                }
+
+
+class IfCmd:
+
+    def gen(self, condition, then, _else):
+        return {'Kind': 'If',
+                'Condition': condition,
+                'Then': then,
+                'Else': _else
+                }
+
+
+class WhileCmd:
+
+    def gen(self, condition, body):
+        return {'Kind': 'While',
+                'Condition': condition,
+                'Body': body
+                }
+
+
+class AssignCmd:
+
+    def gen(self, var, right):
+        return {'Kind': 'Assign',
+                'Left': var,
+                'Right': right
+                }
+
+
+class SeqCmd:
+
+    def gen(self, left, right):
+        return {'Kind': 'Seq',
+                'Left': left,
+                'Right': right
+                }
+
+
+class ExpressionGenerator():
+
+    def gen(self, expr=None, expr_count=0,
+            expr_count_max=randint(1, MAX_EXPRESSIONS_PER_EXPRESSION)):
+        """
+        Return an expression. Even count of expressions within an expression is
+        impossible. In this case the next higher odd count of expressions
+        within an expression is selected.
+        """
+        if expr_count >= expr_count_max:
+            return expr, expr_count
+
+        if expr_count == 0:
+            if expr_count_max == 1:
+                expr, count = self._gen_literal()
+            else:
+                expr, count = self._gen_simpl_expr()
+            expr_count += count
         else:
-            self.ast = self.str().get()
+            if (expr_count + 1) == expr_count_max:
+                expr_next, count = self._gen_literal()
+            else:
+                expr_next, count = ExpressionGenerator().gen(None, 0,
+                                                             expr_count_max -
+                                                             expr_count - 1)
+            rnd = randint(0, 3)
+            if rnd == 0:
+                if randint(0, 1) == 0:
+                    expr = AddExpr().gen(expr, expr_next)
+                else:
+                    expr = AddExpr().gen(expr_next, expr)
+            elif rnd == 1:
+                if randint(0, 1) == 0:
+                    expr = SubExpr().gen(expr, expr_next)
+                else:
+                    expr = SubExpr().gen(expr_next, expr)
+            elif rnd == 2:
+                if randint(0, 1) == 0:
+                    expr = LessExpr().gen(expr, expr_next)
+                else:
+                    expr = LessExpr().gen(expr_next, expr)
+            else:
+                if randint(0, 1) == 0:
+                    expr = EqualExpr().gen(expr, expr_next)
+                else:
+                    expr = EqualExpr().gen(expr_next, expr)
+            expr_count += count + 1
+        expr, count = self.gen(expr, expr_count, expr_count_max)
+        return expr, count
 
-    def get(self):
-        return self.ast
-
-    def int(self):
-        self.ast = TypeExpr('Int', randint(INT_START_RANGE, INT_END_RANGE)).get()
-        return self
-
-    def str(self):
-        self.ast = TypeExpr('Var', self._random_word()).get()
-        return self
-
-    def add(self, e, e2):
-        self.ast = OpExpr('Add', e.get(), e2.get()).get()
-        return self
-
-    def sub(self, e, e2):
-        self.ast = OpExpr('Sub', e.get(), e2.get()).get()
-        return self
-
-    def equal(self, e, e2):
-        self.ast = OpExpr('Equal', e.get(), e2.get()).get()
-        return self
-
-    def less(self, e, e2):
-        self.ast = OpExpr('Less', e.get(), e2.get()).get()
-        return self
-
-    def _random_word(self, n=IDENTIFIER_LENGTH):
-        return ''.join(random.choice(string.ascii_letters) for _ in range(n))
-
-
-def gen_expr(expr):
-    global depth_expression
-    depth_expression += 1
-    if depth_expression >= MAX_DEPTH_EXPRESSION:
-        return expr
-
-    rnd = randint(0, 5)
-    if rnd == 0:  # int
-        if depth_expression == 1:  # set int expr
-            expr.int()
-        else:  # generate new int expression
-            return ExpressionGenerator().int()
-    elif rnd == 1:  # str
-        if depth_expression == 1:  # set str expr
-            expr.str()
-        else:  # generate new str expression
-            return ExpressionGenerator().str()
-    else:
-        e = gen_expr(ExpressionGenerator())
-        e2 = gen_expr(ExpressionGenerator())
-        if rnd == 2:
-            expr.add(e, e2)
-        elif rnd == 3:
-            expr.sub(e, e2)
-        elif rnd == 4:
-            expr.equal(e, e2)
+    def _gen_simpl_expr(self):
+        rnd = randint(0, 5)
+        if rnd == 0 or rnd == 1:
+            return self._gen_literal()
         else:
-            expr.less(e, e2)
-    return expr
+            left, cl = self._gen_literal()
+            right, cr = self._gen_literal()
 
+            if rnd == 2:
+                return AddExpr().gen(left, right), cl + cr + 1
+            elif rnd == 3:
+                return SubExpr().gen(left, right), cl + cr + 1
+            elif rnd == 4:
+                return LessExpr().gen(left, right), cl + cr + 1
+            else:
+                return EqualExpr().gen(left, right), cl + cr + 1
 
-def generate_expression():
-    depth_expression = 0  # set start depth
-    return gen_expr(ExpressionGenerator()).get()
-
-
-class Cmd:
-    kind = None
-
-    def __init__(self, kind):
-        self.kind = kind
-
-
-class IfCmd(Cmd):
-    _condition = None
-    _then = None
-    _else = None
-
-    def __init__(self, e, c, c2=None):
-        Cmd.kind = 'If'
-        self._condition = e
-        self._then = c
-        self._else = c2
-
-    def get(self):
-        return {'Kind': Cmd.kind,
-                'Condition': self._condition,
-                'Then': self._then,
-                'Else': self._else
-                }
-
-
-class WhileCmd(Cmd):
-    _condition = None
-    _do = None
-
-    def __init__(self, e, c):
-        Cmd.kind = 'While'
-        self._condition = e
-        self._do = c
-
-    def get(self):
-        return {'Kind': Cmd.kind,
-                'Condition': self._condition,
-                'Do': self._do
-                }
-
-
-class SomeCmd(Cmd):
-    _left = None
-    _right = None
-
-    def __init__(self, kind, left, right):
-        Cmd.kind = kind
-        self._left = left
-        self._right = right
-
-    def get(self):
-        return {'Kind': Cmd.kind,
-                'Left': self._left,
-                'Right': self._right
-                }
-
-
-class CommandGenerator:
-
-    ast = {}
-
-    def __init__(self):
-        # default cmd is an assignment
-        e = ExpressionGenerator().str().get()
-        e2 = generate_expression()
-        self.assign(e, e2)
-
-    def get(self):
-        return self.ast
-
-    def assign(self, e, e2):
-        self.ast = SomeCmd('Assign', e, e2).get()
-        return self
-
-    def concat(self, c):
-        self.ast = SomeCmd('Concat', c.get(), self.ast).get()
-        return self
-
-    def ifcmd(self, e, c):
-        self.ast = IfCmd(e, c.get(), self.ast).get()
-        return self
-
-    def whilecmd(self, e, c):
-        self.ast = WhileCmd(e, c.get()).get()
-        return self
-
-
-def gen_cmd(cmd):
-    global depth_command
-    depth_command += 1
-    if depth_command >= MAX_DEPTH_COMMAND:
-        return cmd
-
-    rnd = randint(0, 3)
-    if rnd == 0:  # assignment
-        if depth_command == 1:  # cmd is assignment by default
-            True
-        else:  # generate another assignment as cmd
-            return CommandGenerator()
-    else:
-        cmd2 = gen_cmd(CommandGenerator())
-        if rnd == 1:
-            cmd.concat(cmd2)
-        elif rnd == 2:
-            cmd.ifcmd(generate_expression(), cmd2)
+    def _gen_literal(self):
+        if randint(0, 1) == 0:
+            return IntExpr().gen(), 1
         else:
-            cmd.whilecmd(generate_expression(), cmd2)
-    return cmd
+            return VarExpr().gen(), 1
 
 
-def generate_command():
-    depth_command = 0  # set start depth
-    return gen_cmd(CommandGenerator()).get()
+class CommandGenerator():
 
+    def gen(self, cmd=None, cmd_count=0,
+            cmd_count_max=randint(1, MAX_COMMANDS_PER_COMMAND)):
+        if cmd_count >= cmd_count_max:
+            return cmd, cmd_count
 
-class Phrase:
-
-    def generate(self):
-        rnd = randint(0, 1)
-        if rnd == 0:
-            print('Generating Expression as phrase\n')
-            return generate_expression()
+        if cmd_count == 0:
+            expr, _ = ExpressionGenerator().gen()
+            cmd = AssignCmd().gen(VarExpr().gen(), expr)
+            cmd_count += 1
         else:
-            print('Generating Command as phrase\n')
-            return generate_command()
+            if (cmd_count + 1) == cmd_count_max:
+                expr, _ = ExpressionGenerator().gen()
+                cmd = WhileCmd().gen(expr, cmd)
+                cmd_count += 1
+            else:
+                rnd = randint(0, 2)
+                if rnd == 0:
+                    expr, _ = ExpressionGenerator().gen()
+                    cmd = WhileCmd().gen(expr, cmd)
+                    cmd_count += 1
+                elif rnd == 1:
+                    expr, _ = ExpressionGenerator().gen()
+                    cmd_next, count = CommandGenerator().gen(None, 0,
+                                                             cmd_count_max -
+                                                             cmd_count - 1)
+                    if randint(0, 1) == 0:
+                        cmd = IfCmd().gen(expr, cmd, cmd_next)
+                    else:
+                        cmd = IfCmd().gen(expr, cmd_next, cmd)
+                    cmd_count += count + 1
+                else:
+                    cmd_next, count = CommandGenerator().gen(None, 0,
+                                                             cmd_count_max -
+                                                             cmd_count - 1)
+                    if randint(0, 1) == 0:
+                        cmd = SeqCmd().gen(cmd, cmd_next)
+                    else:
+                        cmd = SeqCmd().gen(cmd_next, cmd)
+                    cmd_count += count + 1
+        cmd, count = self.gen(cmd, cmd_count, cmd_count_max)
+        return cmd, count
+
+
+class PhraseGenerator:
+
+    def gen(self):
+        if randint(0, 1) == 0:
+            ast, count = ExpressionGenerator().gen()
+            return 'expr', count, ast
+        else:
+            ast, count = CommandGenerator().gen()
+            return 'cmd', count, ast
 
 
 def prettyprinter(ast):
@@ -269,9 +255,11 @@ def prettyprinter(ast):
         elif ast['Kind'] == 'While':
             code += "while ({}) do {{{}}}".format(
                 prettyprinter(ast['Condition']),
-                prettyprinter(ast['Do']))
+                prettyprinter(ast['Body']))
         elif 'Value' in ast:
             code += str(ast['Value'])
+        elif 'Name' in ast:
+            code += str(ast['Name'])
         else:
             code += "({} {} {})".format(
                 prettyprinter(ast['Left']),
@@ -283,7 +271,7 @@ def prettyprinter(ast):
 def get_center(kind):
     if kind == 'Assign':
         return ':='
-    elif kind == 'Concat':
+    elif kind == 'Seq':
         return '; '
     elif kind == 'Add':
         return '+'
@@ -296,7 +284,8 @@ def get_center(kind):
 
 
 def main():
-    ast = Phrase().generate()
+    _type, count, ast = PhraseGenerator().gen()
+    print('Generated {} with count {}\n'.format(_type, count))
     pprint(ast)
     print('')
     print(prettyprinter(ast))
