2253430cd0ec9c40e6c38e7d33c0999d02abe0c0
Tom Maier <daniel.eggelmann96@googlemail.com>
2019-01-22 10:43:22 +0100

    Test valid and invalid programs at the same time #161

--- final-code/team-2/test_automated.py
+++ final-code/team-2/test_automated.py
@@ -10,50 +10,47 @@ import tokenizer
 
 NO_PROGRAMS = 10000
 MAX_TOKEN_LENGTH = 50
-VALID = True
 IMPLICIT = True
-# threshold value of incorrect classification
-THRESHOLD_PERCENTAGE = 50
 
+def test_programs(count, valid, implicit, validator):
+    no_wrong_classified = 0
+    for i in range(count):
+        tokens = []
+        while (len(tokens) <= 0 or len(tokens) > MAX_TOKEN_LENGTH):
+            ast, _, _ = codegenerator.CommandGenerator().gen(valid, implicit)
+            tokens = tokenizer.Tokenizer().tokenize(ast)
+        x = m.token_to_vec(tokens, MAX_TOKEN_LENGTH)
+        y = validator.predict(x.reshape((1, MAX_TOKEN_LENGTH)))
+
+        res_valid = round(y[0][1] * 100, 2)
+        res_invalid = round(y[0][0] * 100, 2)
+        if (valid and y[0][1] < y[0][0]) or (not valid and y[0][1] > y[0][0]):
+            no_wrong_classified += 1
+            print('\n\n{}. program NOT correctly classified'.format(
+                i))
+            print('Number of tokens: {}'.format(len(tokens)))
+            validStr = str(res_valid)
+            invalidStr = str(res_invalid)
+            print("Valid:  ", formatOutput(validStr, invalidStr), "%")
+            print("Invalid:", formatOutput(invalidStr, validStr), "%")
+            print(codegenerator.prettyprint_multiline_indented(ast))
+    return no_wrong_classified
 
 def main():
-    print('\nTesting {} {} {} programs'.format(NO_PROGRAMS,
-                                               'implicit' if IMPLICIT else 'explicit',
-                                               'valid' if VALID else 'invalid'))
+    print('\nTesting {} {} programs'.format(NO_PROGRAMS,  'implicit' if IMPLICIT else 'explicit'))
     print('Max token length: {}'.format(MAX_TOKEN_LENGTH))
-    print('Threshold percentage of incorrect classification: {}'.format(
-        THRESHOLD_PERCENTAGE))
 
     print("Loading model...")
     path_model = 'models/weights_indirect_flow_iteration2.hdf5'
-    validator = models.LSTMValidator(
-        MAX_TOKEN_LENGTH, len(m.TOKEN2VEC) + 1, "adam")
+    validator = models.LSTMValidator(MAX_TOKEN_LENGTH, len(m.TOKEN2VEC) + 1, "adam")
     validator.load_weights(path_model)
 
-    no_tested = 0
     no_wrong_classified = 0
-    while no_tested < NO_PROGRAMS:
-        ast, _, _ = codegenerator.CommandGenerator().gen(VALID, IMPLICIT)
-        tokens = tokenizer.Tokenizer().tokenize(ast)
-        if len(tokens) <= MAX_TOKEN_LENGTH:
-            no_tested += 1
-            x = m.token_to_vec(tokens, MAX_TOKEN_LENGTH)
-            y = validator.predict(x.reshape((1, MAX_TOKEN_LENGTH)))
+    no_wrong_classified += test_programs(NO_PROGRAMS // 2, True, IMPLICIT, validator)
+    no_wrong_classified += test_programs(NO_PROGRAMS // 2, False, IMPLICIT, validator)
 
-            res_valid = round(y[0][1] * 100, 2)
-            res_invalid = round(y[0][0] * 100, 2)
-            if (not VALID and res_invalid < THRESHOLD_PERCENTAGE) or (VALID and res_valid < THRESHOLD_PERCENTAGE):
-                no_wrong_classified += 1
-                print('\n\n{}. program NOT correctly classified'.format(
-                    no_tested))
-                print('Number of tokens: {}'.format(len(tokens)))
-                validStr = str(res_valid)
-                invalidStr = str(res_invalid)
-                print("Valid:  ", formatOutput(validStr, invalidStr), "%")
-                print("Invalid:", formatOutput(invalidStr, validStr), "%")
-                print(codegenerator.prettyprint_multiline_indented(ast))
     print('\n\n{} of {} wrong classified'.format(
-        no_wrong_classified, no_tested))
+        no_wrong_classified, NO_PROGRAMS))
 
 
 def formatOutput(str, refStr):
